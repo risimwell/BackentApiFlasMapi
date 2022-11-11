f638296a5787f326b7406d8e5b81ac8afc2004d9
CAGG <carlosalbeirogaleano@hotmail.com>
2022-11-11 11:03:34 -0500

    modificacion panel de control acudientes

--- FronteAngular/src/app/Componentes/acudiente/ninos/ninos.component.ts
+++ FronteAngular/src/app/Componentes/acudiente/ninos/ninos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder, Form } from '@angular/forms';
+import { Router } from "@angular/router";
+ 
 @Component({
   selector: 'app-ninos',
   templateUrl: './ninos.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NinosComponent implements OnInit {
 
-  constructor() { }
+  //Capturar datos del formulario
+  formulario_nino =new FormGroup({
+    identificacion: new FormControl('',Validators.required)
+  })
+
+  constructor(private formBuilder:FormBuilder,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  Registrar_nino(form:any){
+    console.log("si manda datos desde el componenete principal")
   }
 
 }
